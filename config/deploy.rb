f0d1b7fbb8aae41b42dc299edebdfb495e07427e
Aaron Manaloto <amanaloto@clinic-it.com>
2015-12-02 17:56:06 +0800

    Add deploy configurations

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'Slack'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:amanaloto/slack-forms.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -23,10 +23,10 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -44,5 +44,13 @@ namespace :deploy do
       # end
     end
   end
+
+  desc "restart (upgrade) unicorn server"
+  task :restart do
+    on roles(:app), :except => {no_release: true} do
+      execute "service unicorn upgrade"
+    end
+  end
+  after :publishing, :restart
 
 end

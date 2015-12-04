b2eec50022c839ec18bd02d60bb84113e3ac6355
Aaron Manaloto <amanaloto@clinic-it.com>
2015-12-04 14:28:06 +0800

    Change slash command method from POST to GET

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root :to => 'sessions#new'
 
   get 'oauth', :to => 'sessions#oauth'
-  post 'slash_command', :to => 'sessions#slash_command'
+  get 'slash_command', :to => 'sessions#slash_command'
 
   resources :forms, :only => [:new, :create]
 
